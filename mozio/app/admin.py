142c25ac3d4fea480a5ef46ad70e4e54f0542a6f
Agustin Toledo <toledoag@gmail.com>
2019-05-25 18:32:16 -0300

    Add models

--- mozio/app/admin.py
+++ mozio/app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
 
-# Register your models here.
+from .models import Provider, ServiceArea
+
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'telphone', 'language', 'currency')
+
+
+@admin.register(ServiceArea)
+class ServiceAreaAdmin(OSMGeoAdmin):
+    list_display = ('name', 'price', 'area')
--- mozio/app/models.py
+++ mozio/app/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from languages.fields import LanguageField
+from djmoney.models.fields import MoneyField
 
-# Create your models here.
+from django.db import models
+from django.contrib.gis.db.models import PolygonField
+
+
+class Provider(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField()
+    telphone = models.CharField(max_length=80)
+    language = LanguageField()
+    currency = models.CharField(max_length=3)
+
+
+class ServiceArea(models.Model):
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    price = MoneyField(decimal_places=3, max_digits=8)
+    area = PolygonField()
+
+    def __str__(self):
+        return f'{self.name} ({self.price})'
